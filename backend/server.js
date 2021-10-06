fa7abcd0cfce4725df162eb714696f07d604227f
unknown <chenjuexu007@gmail.com>
2021-10-06 12:53:22 -0400

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,7 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-
-
 dotenv.config()
-
 connectDB()
 const app = express()
 
@@ -28,19 +25,25 @@ app.use('/products', productRoutes)
 app.use('/users', userRoutes)
 app.use('/orders', orderRoutes)
 app.use('/upload', uploadRoutes)
-
-
-
-
 app.get('/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 app.use(notFound)
 app.use(errorHandler)
-
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT,console.log(`Server running  in ${process.env.NODE_ENV} mode on port ${PORT}`))
